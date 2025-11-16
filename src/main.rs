7cd965533de9cf0ad1ae9a0740cf498141b91fa6
wlcsv <wlcsv8080@outlook.com>
2025-11-16 17:05:21 -0300

    Agora retorna o tipo correto.

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
 use ndarray::*;   
 
 fn main() {
-    let a = arr1(&[30.0]);
+    let a = arr2(&[[0.0]]);
 
     let a_negative = negative(a);
 
@@ -10,10 +10,14 @@ fn main() {
 
 // Logic
 
-fn negative(value: Array1<f32>) -> f32 {
-
-    arr1(&[-1.0]).dot(&value)
+fn negative(value: Array2<f32>) -> Array2<f32> {
+    arr2(&[[-1.0]]).dot(&value) + arr2(&[[1.0]])
 }
 
+fn and(value1: Array2<f32>, value2: Array2<f32>) -> Array2<f32> {
+    value1.dot(&value2)
+}
 
-
+fn or(value1: Array2<f32>, value2: Array2<f32>) -> Array2<f32> {
+    &value1 + &value2 - value1.dot(&value2)
+}
